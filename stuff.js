f714a0ca6a1071eec14b562e7be18e4ebc8fe0f8
Garrett Bourg <baojr@upe.wustl.edu>
2014-08-02 18:35:14 -0700

    Adds content to sponsors, FAQ, about, and application sections

--- stuff.js
+++ stuff.js
@@ -1,15 +1,22 @@
-$(document).ready(function() {
+﻿$(document).ready(function() {
   var mainDiv = $('#main');
   var defaultButton = $('#show-default');
   var aboutButton = $('#show-about');
   var sponsorsButton = $('#show-sponsors');
+  var faqButton = $('#show-faq');
+  var applyButton = $('#show-apply');
+
 
   var defaultDiv = $('#default-content');
   var aboutDiv = $('#about-content');
   var sponsorsDiv = $('#sponsors-content');
+  var faqDiv = $('#faq-content');
+  var applyDiv = $('#apply-content');
 
 
-  var terminalText = '>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  var terminalText = '&gt;Washington University in St. Louis\'s Upsilon Pi Epsilon is hosting a hackathon open to all college students this fall<br>' +
+                     '&gt;The hackathon will run from September 12th — 14th and will have top companies such as Google and Microsoft as sponsors<br>' +
+                     '&gt;Submit an application to register!';
   var timer;
 
   function show(div) {
@@ -25,11 +32,13 @@ $(document).ready(function() {
       var delay = Math.random() * 5 + 15;
       if (terminalText.charAt(i) == ' ') {
         delay += 30;
+      } else if (terminalText.charAt(i) == '\n') {
+      
       }
       timer = setTimeout(function() {
-        terminal.text(terminalText.substring(0, ++i) + '_');
+        terminal.html(terminalText.substring(0, ++i) + '_');
         if (i == terminalText.length) {
-          terminal.text(terminalText);
+          terminal.html(terminalText);
           clearTimeout(timer);
         } else { 
           addCharacter();
@@ -48,6 +57,8 @@ $(document).ready(function() {
   defaultButton.click(function() { show(defaultDiv); startTyping(); });
   aboutButton.click(function() { show(aboutDiv); });
   sponsorsButton.click(function() { show(sponsorsDiv); });
+  faqButton.click(function() { show(faqDiv); });
+  applyButton.click(function() { show(applyDiv); });
   
   show(defaultDiv);
   startTyping();
